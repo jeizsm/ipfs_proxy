d3e75ca4198ca704457680ca0c0fbdad52a2d474
Marat Safin <jeizsm@gmail.com>
2021-09-02 16:14:06 +0300

    streaming proxy

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,22 @@
-use actix_web::{body, client::Client, middleware, web, App, HttpRequest, HttpServer, Responder};
+mod proxy;
+use crate::proxy::proxy;
+use actix_cors::Cors;
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
 
-async fn proxy(req: HttpRequest, mut body: web::Bytes) -> impl Responder {
-    let new_uri = format!("http://localhost:5001{}", req.path());
-    let result = Client::default()
-        .request_from(&new_uri, req.head())
-        .set_header("host", "localhost:5001")
-        .send_body(body)
-        .await;
-    result.unwrap().body().await
-    //format!("{:?}, {:?}, {}", result, req.head(), &new_uri)
-    //.send_body(req.to_owned);
-    // format!("{}", req.path())
-    //"hello ipfs\n"
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allow_any_header()
+            .allow_any_method();
         App::new()
-            .wrap(middleware::NormalizePath::default())
+            .wrap(cors)
             .service(web::scope("/api/v0").default_service(web::route().to(proxy)))
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
